fc3e6d5c5a19948b63c50e1101645c1617c53d0d
Maria Lobanova <roldik95@gmail.com>
2021-03-10 22:01:43 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
-}
+        if(n<0){
+          n*=(-1);
+        }
+         let str = String(n);
+         let reverStr = "";
+         for (let i = (str.length-1); i>=0; i--){
+              reverStr+=str[i];
+         }
+         return Number(reverStr);
+       }
+      
+
