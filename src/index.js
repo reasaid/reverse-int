3ea49df05f0cff35526c74c1a34a3e08af5125d3
Maria Lobanova <roldik95@gmail.com>
2021-08-04 18:44:41 +0300

    refactor: change absolute value

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 module.exports = function reverse(n) {
-    if (n < 0) {
-        n *= -1;
-    }
-    return Number(String(n).split("").reverse().join(""));
+    return Number(String(Math.abs(n)).split("").reverse().join(""));
 };
